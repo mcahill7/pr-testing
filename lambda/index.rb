3af13682418169f7db01c9dade5f006dbd2d6212
Mason Cahill <mason.j.cahill@gmail.com>
2019-05-28 12:42:04 -0400

    Update codebuild project, add in skeleton of lambda (#1)
    * test lambda
    
    * add lambda sam

--- lambda/index.rb
+++ lambda/index.rb
@@ -2,5 +2,17 @@ require 'json'
 require 'aws-sdk'
 
 def handler(event:, context:)
+    begin
+        github_token = ENV['GITHUB_TOKEN']
+
+        if event['headers'].include? 'X-GitHub-Event'
+            puts "true"
+        else
+            puts "false"
+        end
+
+    rescue
+        puts "No Event"
+    end
 
 end
