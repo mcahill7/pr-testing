fe505c01e863582194987be05c2b85a2c511b631
Mason Cahill <mason.j.cahill@gmail.com>
2019-06-13 15:00:02 -0400

    Delete githook.sh

--- tasks/githook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl "https://api.github.com/repos/$0/$1/statuses/$2?access_token=$3" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"context\": \"#Coverage Change\", \"state\": \"success\", \"description\": \"$4\"}"
